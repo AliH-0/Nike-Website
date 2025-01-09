e349c6ad8080f26c62551fcfe4e1e00589afa0f6
AliH-0 <ali.h.husseini0@gmail.com>
2025-01-09 18:29:45 +0200

    Set up main structure and sections for App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,17 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+const App = () => (
+  <main className="relative">
+    Nav
+    <section className="xl:padding-1 wide:padding-r padding-b">Hero</section>
+    <section className="padding">Popular Products</section>
+    <section className="padding">Super Quality </section>
+    <section className="padding">PServices</section>
+    <section className="padding">Special Offers</section>
+    <section className="bg-pale-blue padding">Customer Reviews</section>
+    <section className="padding-x sm:py-32 py-16 w-full">Subdcribe</section>
+    <section className="padding bg-black padding-x padding-pb-8">
+      Footer
+    </section>
+  </main>
+);
+
+export default App;
