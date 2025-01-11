a00d1d06a07387afac2526aaa32e3c7a75bbf794
AliH-0 <ali.h.husseini0@gmail.com>
2025-01-11 23:25:24 +0200

    Finished customer reviews section

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -1,3 +1,6 @@
+import ReviewCard from "../components/ReviewCard"
+import { reviews } from "../constants"
+
 const CustomerReviews = () => {
   return (
     <section className="max-container">
@@ -10,8 +13,18 @@ const CustomerReviews = () => {
         Hear genuine stories from our satisfied customers about their
         expierences with us.
       </p>
-      {/* justify evenly distribute space between and around flex items.*/}
-      <div className="mt-24 flex flex-1 justify-evenly "></div>
+      {/* justify evenly makes it so theres an equal around of space between each item*/}
+      <div className="mt-24 flex flex-1 justify-evenly items-center max-lf:flex-col gap-14">
+        {reviews.map((review) => (
+          <ReviewCard
+            key={review.customerName}
+            imgURL={review.imgURL}
+            customerName={review.customerName}
+            rating={review.rating}
+            feedback={review.feedback}
+          />
+        ))}
+      </div>
     </section>
   )
 }
